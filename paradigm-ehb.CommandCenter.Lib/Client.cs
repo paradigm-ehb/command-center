bef3819a81b5dc5120b18a520997954db6c5426d
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-11-25 17:59:52 +0100

    Add project files.

---
+++ paradigm-ehb.CommandCenter.Lib/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace paradigm_ehb.CommandCenter.Lib
+{
+    public class Client
+    {
+        public static string DependencyMethod()
+        {
+            return "Hello from CommandCenter.Lib!";
+        }
+    }
+}
---
+++ paradigm-ehb.CommandCenter.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace paradigm_ehb.CommandCenter.WinUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog dialog = new ContentDialog
+            {
+                Title = "Hello from WinUI!",
+                Content = Client.DependencyMethod(),
+                CloseButtonText = "OK",
+                XamlRoot = this.Content.XamlRoot
+            };
+
+            ContentDialogResult result = await dialog.ShowAsync();
+        }
+    }
+}
