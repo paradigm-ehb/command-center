245ca2c37abf749a78b601550fa2c889b5cbd319
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-11-26 16:18:33 +0100

    feat: add basic grpc greet method

--- paradigm-ehb.CommandCenter.Lib/Client.cs
+++ paradigm-ehb.CommandCenter.Lib/Client.cs
@@ -1,10 +1,23 @@
-﻿namespace paradigm_ehb.CommandCenter.Lib
+﻿using Grpc.Net.Client;
+
+namespace paradigm_ehb.CommandCenter.Lib
 {
     public class Client
     {
         public static string DependencyMethod()
         {
             return "Hello from CommandCenter.Lib!";
+        }
+
+        public static async Task<string> GrpcGreetAsync(string address)
+        {
+            GrpcChannel channel = GrpcChannel.ForAddress(address);
+            Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+
+            HelloReply response = await client.SayHelloAsync(
+                new HelloRequest { Name = "World" });
+
+            return response.Message;
         }
     }
 }
