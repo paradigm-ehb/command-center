f70f51f6c1674596e5276376fa89ef0138ea765c
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-12-06 12:39:00 +0100

    Refactor Client.cs to remove unused dependencies
    Removed unused `using` directives for `Grpc.Net.Client` and
    `paradigm_ehb.CommandCenter.Core.Services`. Reformatted the
    `namespace` declaration to use block-style braces. Simplified
    `DependencyMethod` by removing the unused `GrpcClientFactory`
    instantiation and directly returning the result string.

--- src/paradigm-ehb.CommandCenter.Core/Client.cs
+++ src/paradigm-ehb.CommandCenter.Core/Client.cs
@@ -1,13 +1,9 @@
-﻿using Grpc.Net.Client;
-using paradigm_ehb.CommandCenter.Core.Services;
-
-namespace paradigm_ehb.CommandCenter.Core
+﻿namespace paradigm_ehb.CommandCenter.Core
 {
     public class Client
     {
         public static string DependencyMethod()
         {
-            GrpcClientFactory grpcClientFactory = new();
             return "Hello from CommandCenter.Lib!";
         }
     }
