1a2aab969e30edcf829a996e3b181d1483fc415b
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-12-06 12:36:25 +0100

    Add DependencyInjection class for DI configuration
    Introduced a `DependencyInjection` class in the `paradigm_ehb.CommandCenter.Core` namespace to streamline dependency injection setup.
    
    The `AddCommandCenterCore` method was added to extend `IServiceCollection`, registering the following services as singletons:
    - `IAgentEndpointFactory` -> `AgentEndpointFactory`
    - `IAgentRegistry` -> `AgentRegistryService`
    - `IGrpcChannelFactory` -> `GrpcChannelFactory`

---
+++ src/paradigm-ehb.CommandCenter.Core/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using paradigm_ehb.CommandCenter.Core.Factories;
+using paradigm_ehb.CommandCenter.Core.Interfaces;
+using paradigm_ehb.CommandCenter.Core.Services;
+
+namespace paradigm_ehb.CommandCenter.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCommandCenterCore(this IServiceCollection services)
+        {
+            services.AddSingleton<IAgentEndpointFactory, AgentEndpointFactory>();
+            services.AddSingleton<IAgentRegistry, AgentRegistryService>();
+            services.AddSingleton<IGrpcChannelFactory, GrpcChannelFactory>();
+            return services;
+        }
+    }
+}
