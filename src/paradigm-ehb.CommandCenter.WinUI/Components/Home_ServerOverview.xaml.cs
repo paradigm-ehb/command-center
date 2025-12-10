c781fe2ee6521d83204913972afc166afc826e65
imadofficial <git config user.email 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>
2025-12-10 11:54:58 +0100

    Added a new property to the Home__ServerOverview "Status" (int) & added server creation window (with CTRL+N)

---
+++ src/paradigm-ehb.CommandCenter.WinUI/Components/Home_ServerOverview.xaml.cs
@@ -0,0 +1,100 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using static System.Net.WebRequestMethods;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace paradigm_ehb.CommandCenter.WinUI.Components.Reusable;
+
+public sealed partial class Home_ServerOverview : UserControl
+{
+    public Home_ServerOverview()
+    {
+        InitializeComponent();
+
+        rootBorder.PointerEntered += (s, e) =>
+        {
+            VisualStateManager.GoToState(this, "PointerOver", true); //Starts the animation
+        };
+
+        rootBorder.PointerExited += (s, e) =>
+        {
+            VisualStateManager.GoToState(this, "Normal", true); //Ends the animation
+        };
+    }
+
+    private void setupStatus(int Status)
+    {
+        switch(Status)
+        {
+            case 0:
+                setText(Windows.UI.Color.FromArgb(255, 184, 6, 6), "Offline");
+                break;
+            case 1:
+                setText(Windows.UI.Color.FromArgb(255, 255, 111, 0), "Degraded");
+                break;
+            case 2:
+                setText(Windows.UI.Color.FromArgb(255, 138, 138, 138), "Unknown");
+                break;
+            case 3:
+                setText(Windows.UI.Color.FromArgb(255, 105, 168, 54), "Online");
+                break;
+        }
+    }
+
+    private void setText(Windows.UI.Color kleur, String text)
+    {
+        StatusColor.Fill = new SolidColorBrush(kleur);
+        StatusText.Text = text;
+    }
+
+    public string ServerName
+    {
+        get => (string)GetValue(ServerNameProperty);
+        set => SetValue(ServerNameProperty, value);
+    }
+
+    public int ServerStatus
+    {
+        get => (int)GetValue(ServerStatusProperty);
+        set => SetValue(ServerStatusProperty, value);
+    }
+
+    public static readonly DependencyProperty ServerNameProperty = 
+        DependencyProperty.Register(
+            nameof(ServerName),          // Property name
+            typeof(string),              // Property Datatype
+            typeof(Home_ServerOverview), // Coming from...
+            new PropertyMetadata(null)); // Default Value
+
+    public static readonly DependencyProperty ServerStatusProperty =
+    DependencyProperty.Register(
+        nameof(ServerStatus),
+        typeof(int),
+        typeof(Home_ServerOverview),
+        new PropertyMetadata(0, OnServerStatusChanged));
+
+    private static void OnServerStatusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (Home_ServerOverview)d;
+        control.setupStatus((int)e.NewValue);
+    }
+
+
+
+}
--- src/paradigm-ehb.CommandCenter.WinUI/HomePage.xaml.cs
+++ src/paradigm-ehb.CommandCenter.WinUI/HomePage.xaml.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +6,13 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -32,15 +33,15 @@ public sealed partial class HomePage : Page
     {
         args.Handled = true;
 
-        var dialog = new ContentDialog
+        ContentDialog noWifiDialog = new ContentDialog
         {
-            Title = "Shortcut Activated",
-            Content = "You pressed CTRL + N!",
+            Title = "Add a new server",
+            Content = new ServerCreation(),
             CloseButtonText = "OK",
-            XamlRoot = this.Content.XamlRoot
+            XamlRoot = this.XamlRoot
         };
 
-        await dialog.ShowAsync();
+        ContentDialogResult result = await noWifiDialog.ShowAsync();
     }
 
 }
