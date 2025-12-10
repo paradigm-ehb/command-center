d2a5108fa0975deb9bf7f93844d9c622e75f3e04
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-12-10 13:32:13 +0100

    fix: remove GrpcGreetAsync method from Client.cs
    The GrpcGreetAsync method, responsible for making a gRPC call to the Greeter service, has been removed from Client.cs. This eliminates the related gRPC channel and client creation, as well as the HelloRequest and response handling logic.

--- src/paradigm-ehb.CommandCenter.Core/Client.cs
+++ src/paradigm-ehb.CommandCenter.Core/Client.cs
@@ -6,16 +6,5 @@
         {
             return "Hello from CommandCenter.Lib!";
         }
-
-        public static async Task<string> GrpcGreetAsync(string address)
-        {
-            GrpcChannel channel = GrpcChannel.ForAddress(address);
-            Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
-
-            HelloReply response = client.SayHello(
-                new HelloRequest { Name = "World" });
-
-            return response.Message;
-        }
     }
 }
