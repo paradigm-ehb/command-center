2675a40c81c45fd1d44fd9409c83eef103120f9e
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-12-10 14:57:15 +0100

    test: add unit tests; make AgentEndpointFactory public sealed;
    Changed AgentEndpointFactory to public sealed for wider access and to prevent inheritance. Added AgentEndpointFactoryTest.cs with unit tests to verify correct creation of AgentEndpoint instances with all parameters.

---
+++ tests/paradigm-ehb.CommandCenter.Core.Tests/AgentEndpointFactoryTest.cs
@@ -0,0 +1,36 @@
+﻿using paradigm_ehb.CommandCenter.Core.Factories;
+using paradigm_ehb.CommandCenter.Core.Models;
+
+namespace paradigm_ehb.CommandCenter.Core.Tests
+{
+    public class AgentEndpointFactoryTest
+    {
+        AgentEndpointFactory _agentEndpointFactory = new AgentEndpointFactory();
+
+        [Fact]
+        public void CreateEndpointWithAllParams()
+        {
+            string ipAddress = "192.168.1.1";
+            int port = 4242;
+            bool useTls = false;
+            string displayName = "Server01";
+            Dictionary<string, string> metadata = new Dictionary<string, string>()
+            {
+                ["os"] = "windows",
+                ["version"] = "11.22.33",
+                ["foo"] = "bar"
+            };
+
+            AgentEndpoint agentEndpoint = _agentEndpointFactory.Create(ipAddress, port, useTls, displayName, metadata);
+
+            Assert.NotNull(agentEndpoint);
+            Assert.Equal(ipAddress, agentEndpoint.IpAddress);
+            Assert.Equal(port, agentEndpoint.Port);
+            Assert.Equal(useTls, agentEndpoint.UseTls);
+            Assert.Equal(displayName, agentEndpoint.DisplayName);
+            Assert.Equal(metadata, agentEndpoint.Metadata);
+            Assert.Null(agentEndpoint.LastSeen);
+            Assert.Equal(AgentHealthStatus.Unknown, agentEndpoint.HealthStatus);
+        }
+    }
+}
